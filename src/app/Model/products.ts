debe6b7c455789702056083b35ddf0c5e4cf68f0
TRILOK CHAND SWAMI <ittiku3@outlook.com>
2025-02-06 21:50:34 +0530

    Working on the API's now

--- src/app/Model/products.ts
+++ src/app/Model/products.ts
@@ -1,2 +1,8 @@
 export interface Products {
+    ProductItemID: number,
+    Price: number,
+    Rating: number,
+    ProductName: string,
+    Description: string,
+    Status: boolean
 }
--- src/app/Service/product.service.ts
+++ src/app/Service/product.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Products } from '../Model/products';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  // Create the URL of the API
+  private apiUrl = '';
+
   constructor() { }
+
+  // Inject the Http 
+  http = Inject(HttpClient);
+
+  getAllProduct() {
+    return this.http.get(this.apiUrl) as Observable<Products[]>;
+  }
+
 }
